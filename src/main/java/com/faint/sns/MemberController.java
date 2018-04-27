aa0c9e94641702ef32c22b50e799627e62faa041
DESKTOP-SNIKORI\PARK <parkmh8484@gmail.com>
2018-04-27 21:59:40 +0900

    비공개 삭제, 게시글 수정 css 수정함

--- src/main/java/com/faint/sns/MemberController.java
+++ src/main/java/com/faint/sns/MemberController.java
@@ -205,7 +205,6 @@ public class MemberController {
 			//유저 id로 갱신된 데이터 새로 읽기
 			user = service.read(user.getId());
 			logger.info(user.toString());
-			user.setIntro(user.getIntro().replaceAll(" <br> ", "\n"));
 			model.addAttribute("userVO", user);
 			model.addAttribute("reqURL", request.getRequestURI());
 		}
